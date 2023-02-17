c75bf5db7b453af391468ede51ecc68ec2dfce35
Nusret Atıf GÖbütoğlu <nusretatif08@gmail.com>
2023-02-17 21:05:35 +0300

    time.sleep() came back, explicit timer will add

--- Project/main.py
+++ Project/main.py
@@ -4,8 +4,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Keys
 import urllib.request
-import numpy as np
-from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def login(USERNAME,PASS):
@@ -19,7 +18,9 @@ def login(USERNAME,PASS):
     button.click()
 
 def closeModal() :
-    closeButton=wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.close")))
+    time.sleep(3)
+    # closeButton=wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.close")))
+    closeButton = driver.find_element(By.CSS_SELECTOR,"button.close")
     closeButton.click()
     global scrollPosition
     scrollPosition += 100
@@ -32,29 +33,53 @@ service = Service("./chromedriver")
 driver = webdriver.Chrome(options= chrome_options,service=service)
 driver.maximize_window()
 scrollPosition = 0
-wait = WebDriverWait(driver, 10)
+# wait = WebDriverWait(driver, 10)
 
-datafromfile=np.loadtxt("LectureURLS.txt",dtype="str")
-wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"li.active a")))
-
+datafromfile=["https://online.yildiz.edu.tr/?transaction=LMS.EDU.LessonProgram.ViewOnlineLessonProgramForStudent/36484"]
 login("<your e-mail adress>","<password>")
-
+# wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"li.active a")))
+time.sleep(3)
 for DOWNLOAD_URL in datafromfile:
     scrollPosition = 0
     driver.get(DOWNLOAD_URL)
-    wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,".btn.btn-xs.btn-info")))
+    # wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,".btn.btn-xs.btn-info")))
+    time.sleep(3)
     lectureName = driver.find_element(By.CSS_SELECTOR,".col-sm-8.grid8").get_attribute("innerHTML") #look
     allVideos = driver.find_elements(By.CSS_SELECTOR,".btn.btn-xs.btn-info")
     for video in allVideos:
         try:
-            wait.until_not(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.close")))
+            time.sleep(3)
+            # wait.until_not(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.close")))
             video.click()
-            videoFile = driver.find_element(By.TAG_NAME,"a").get_attribute("href")
-            videoName = driver.find_element(By.CSS_SELECTOR,"td[title='Başlangıç Zamanı']").get_attribute("innerHTML") #look
-            videoName = lectureName.strip() + " " + videoName.split(" ")[0]+".mp4"   
-            urllib.request.urlretrieve(videoFile,videoName)       #Video download process
-            closeModal()
+            time.sleep(3)
+            # wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.close")))
+            videoFile = driver.find_elements(By.XPATH,"//a[starts-with(text(),'İzle')]")
+            print(videoFile)
+            print(len(videoFile))
+            if (len(videoFile)>1):
+                idx=0
+                for v in videoFile:
+                    idx+=1
+                    videoFile= v.get_attribute("href")
+                    videoName = driver.find_element(By.CSS_SELECTOR,"td[title='Başlangıç Zamanı']").get_attribute("innerHTML") #look
+                    videoName = lectureName.strip() + " " + videoName.split(" ")[0]+ "_({})".format(idx)+".mp4"  
+                    print(videoName+" Basladı") 
+                    time.sleep(5)
+                    # urllib.request.urlretrieve(videoFile,videoName)       #Video download process              
+                closeModal()
+                idx=0
+            else:
+                videoFile = videoFile[0].get_attribute("href")
+                videoName = driver.find_element(By.CSS_SELECTOR,"td[title='Başlangıç Zamanı']").get_attribute("innerHTML") #look
+                print(videoName+"Basladı")
+                videoName = lectureName.strip() + " " + videoName.split(" ")[0]+".mp4"   
+                time.sleep(5)
+                # urllib.request.urlretrieve(videoFile,videoName)       #Video download process
+                closeModal()
         except:
-            closeModal()
+            try :
+                closeModal()
+            except:
+                driver.close()
 
         
